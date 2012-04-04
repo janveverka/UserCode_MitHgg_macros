20449372b76757faf4828410fa5850dfedf58b74
bendavid <>
2012-04-04 12:53:34 +0000

    minor fixes

--- dumptxt.C
+++ dumptxt.C
@@ -1,13 +1,24 @@
-void dumptxt
+#include "TString.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TTreePlayer.h"
+#include "TCut.h"
+
+void dumptxt()
 {
- TString pathAndFile    =TString("/scratch/bendavid/root/hggmvaMar19/appOutput_SMmet_Feb16.root");
+  TString pathAndFile    =TString("/scratch/bendavid/root/hggmvaMar19/appOutput_SMmet_Feb16.root");
   
   TFile* file = TFile::Open(pathAndFile.Data());
   TTree* tup = (TTree*) file->FindObjectAny("MITMVAtuple");
 
+  ((TTreePlayer*)(tup->GetPlayer()))->SetScanRedirect(true);
+  ((TTreePlayer*)(tup->GetPlayer()))->SetScanFileName("syncdump.txt");  
+  tup->SetScanField(0);
+
   TCut scancut = "proc==11 && pt1>(mass/3.0) && pt2>(mass/4.0) && pt1>(100.0/3.0) && pt2>(100.0/4.0) && mass > 100. && idmva1>-0.3 && idmva2>-0.3 && mass>100 && mass<180";
   
   tup->Scan("run:lumi:evt:mass:ptgg:pt1/mass:pt2/mass:eta1:eta2:cos(phi1-phi2):vtxprob:res:reswrong:idmva1:idmva2:e1:e2:relresraw1:relresraw2:etcorecaliso1:etcorecaliso2:etcorhcaliso1:etcorhcaliso2:etcortrkiso1:etcortrkiso2:abstrkisocic1:abstrkisocic2:mva",scancut,"colsize=10");
- return;
+  
+  return;
 
 }
