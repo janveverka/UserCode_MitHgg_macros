a4555e99daae65a1dcd35c96de32466b3cb17fd2
mingyang <>
2012-06-19 14:19:56 +0000

    train use globe ntuple with problem diphotonjets events remove

--- trainTMVA_Bambu_MIT.C
+++ trainTMVA_Bambu_MIT.C
@@ -23,46 +23,6 @@
 
 #endif
 
-// MIT BDT Training for H->gg analysis
-
-/*double trigEff[36];
-trigEff[0] = 0.990  ;   
-trigEff[1] = 0.989  ;
-trigEff[2] = 0.977  ; 
-trigEff[3] = 0.977  ; 
-trigEff[4] = 0.987  ; 
-trigEff[5] = 0.991  ; 
-trigEff[6] = 0.968  ; 
-trigEff[7] = 0.973  ; 
-trigEff[8] = 0.986  ; 
-trigEff[9] = 0.975  ; 
-trigEff[10] = 0.975  ;
-trigEff[11] = 0.985  ;
-trigEff[12] = 0.988  ;
-trigEff[13] = 0.967  ;
-trigEff[14] = 0.969  ;
-trigEff[15] = 0.964  ;
-trigEff[16] = 0.964  ;
-trigEff[17] = 0.974  ;
-trigEff[18] = 0.977  ;
-trigEff[19] = 0.952  ;
-trigEff[20] = 0.958  ; 
-trigEff[21] = 0.963  ;
-trigEff[22] = 0.973  ;
-trigEff[23] = 0.974  ;
-trigEff[24] = 0.951  ;
-trigEff[25] = 0.956  ;
-trigEff[26] = 0.984  ;
-trigEff[27] = 0.987  ;
-trigEff[28] = 0.964  ;
-trigEff[29] = 0.969  ;
-trigEff[30] = 0.986  ;
-trigEff[31] = 0.968  ;
-trigEff[32] = 0.967  ;
-trigEff[33] = 0.942  ;
-trigEff[34] = 0.948  ;
-trigEff[35] = 0.949  ;*/
-
 TH1D *getpuweights(TFile *file, TH1D *target) {
   
   TDirectory *dirmcpv = (TDirectory*)file->FindObjectAny("AnaFwkMod");
@@ -129,10 +89,18 @@ float effweight(int cat) {
 
   //2012 Jan16 freeze numbers(ming: accroding to Matteo's email
   //scale factor for the effieicnecy of the single photon
-  if (cat==1) return 0.999;
-  else if (cat==2) return 0.984;
-  else if (cat==3) return 1.006;
-  else if (cat==4) return 1.014;
+  //if (cat==1) return 0.999;
+  //else if (cat==2) return 0.984;
+  //else if (cat==3) return 1.006;
+  //else if (cat==4) return 1.014;
+  //else return 1.0;  
+
+  //2012 ICHEP Open Box
+  //scale factor for the effieicnecy of the single photon
+  if (cat==1) return 1.0;
+  else if (cat==2) return 1.0;
+  else if (cat==3) return 1.0;
+  else if (cat==4) return 1.0;
   else return 1.0;  
 }
 
@@ -160,260 +128,168 @@ float trigeffweight(int cat1, int cat2, double pt1, double pt2) {
   
 }
 
-
-// float trigeffweight(int cat1, int cat2, double pt1, double pt2) {
-// 
-//   Bool_t isb1 = cat1==1 || cat1==2;
-//   Bool_t isb2 = cat2==1 || cat2==2;
-//   
-//   Bool_t isr91 = cat1==1 || cat1==3;
-//   Bool_t isr92 = cat2==1 || cat2==3;
-// 
-//   int _cat1 = 1;
-//   if ( !isb1 ) _cat1 += 4;
-//   if ( !isr91 ) _cat1 += 2;
-//   if ( pt1 < 40./120.*100.) _cat1 += 1;
-// 
-//   int _cat2 = 1;
-//   if ( !isb2 ) _cat2 += 4;
-//   if ( !isr92 ) _cat2 += 2;
-//   if ( pt2 < 40./120.*100.) _cat2 += 1;
-// 
-//   if( _cat2 < _cat1) {
-//     int tmp = _cat2;
-//     _cat2 = _cat1;
-//     _cat1 = tmp;
-//   }
-// 
-//   int binNum = ( _cat1 - 1 ) * ( 18 - _cat1 )/2 + ( _cat2 - _cat1 );
-// 
-//   return trigEff[binNum];
-// }
-
-//ming: this setRelErrAlias seems not be used
-void setRelErrAlias(TTree* tree) {
-  TString  corfact = "0.07";
-  TString relerrsmearcor = TString::Format("0.5*sqrt( (pow(1.0+ismc*ph1.isbarrel*%s,2)*ph1.eerr*ph1.eerr+ph1.esmearing*ph1.esmearing)/ph1.e/ph1.e + (pow(1.0+ismc*ph2.isbarrel*%s,2)*ph2.eerr*ph2.eerr+ph2.esmearing*ph2.esmearing)/ph2.e/ph2.e)",corfact.Data(),corfact.Data());
-
-  tree->SetAlias("massrelerr",relerrsmearcor.Data());
-  return;
-}
-
 void trainTMVA_Bambu_MIT() {
   
   //-------------set up the weights-------------------
   //pu target
   //TFile *filepuest = new TFile("/scratch/bendavid/root/puweightsNov13/2011_0100_73500.pileup.root","READ");
-  TFile *filepuest = new TFile("/home/mingyang/cms/puweight/augmented_nov08_rereco.json.68000.pileup.root","READ");
-  TH1D *hpuest = (TH1D*) filepuest->Get("pileup");
-
+  //TFile *filepuest = new TFile("/home/mingyang/cms/puweight/augmented_nov08_rereco.json.68000.pileup.root","READ");
+  //TH1D *hpuest = (TH1D*) filepuest->Get("pileup");
   
   //load higgs pt-weights
-  TFile *fileptweight = new TFile("/scratch/bendavid/root/KFactors_AllScales.root","READ");
+  //TFile *fileptweight = new TFile("/scratch/bendavid/root/KFactors_AllScales.root","READ");
   //TFile *fileptweight = new TFile("./root/KFactors_AllScales.root","READ");
-  ptweights= (TH1D*) fileptweight->Get("kfact125_0");//ming: how about higgs 123  
+  //ptweights= (TH1D*) fileptweight->Get("kfact125_0");//ming: how about higgs 123  
 
   //-------------get input tree-------------------
-  //TString prefix = "rfio:///castor/cern.ch/user/f/fabstoec/BambuYstar_Oct14/";  
-  TString prefix = "/home/mingyang/cms/hist/hgg-v0/merged/";
-
-  TString signal_1 ="hgg-v0_f11--h123gg-gf-v14b-pu_noskim.root";
-  TString signal_2 ="hgg-v0_f11--h123gg-vbf-v14b-pu_noskim.root";
-  TString signal_3 ="hgg-v0_f11--h123gg-vh-v14b-pu_noskim.root";
-  TString signal_4 ="hgg-v0_f11--h123gg-tt-v14b-pu_noskim.root";
-
-  TString bgBorn   ="hgg-v0_f11--diphoj-v14b-pu_noskim.root";
-  TString bgBox_1  ="hgg-v0_f11--2pibx10_25-v14b-pu_noskim.root";
-  TString bgBox_2  ="hgg-v0_f11--2pibx25_250-v14b-pu_noskim.root";
-  TString bgBox_3  ="hgg-v0_f11--2pibx250-v14b-pu_noskim.root";
-
-  TString bgQCDl   ="hgg-v0_f11--qcd-2em3040-v14b-pu_noskim.root";
-  TString bgQCD    ="hgg-v0_f11--qcd-2em40-v14b-pu_noskim.root";
-  TString bgPJ     ="hgg-v0_f11-pj-2em20-v14b-pu_noskim.root";
-
+  TString prefix = "/home/mingyang/cms/root/RootFiles/2012_ICHEP/DiphotonMVA/";
+  TString rootfile ="diphomva_training_dijet.root";
+  
   std::cout<<"  loading files... "<<std::endl;
 
-  TFile* f_signal_1 = TFile::Open((prefix+signal_1).Data());
-  TFile* f_signal_2 = TFile::Open((prefix+signal_2).Data());
-  TFile* f_signal_3 = TFile::Open((prefix+signal_3).Data());
-  TFile* f_signal_4 = TFile::Open((prefix+signal_4).Data());
-
-  TFile* f_bgBorn   = TFile::Open((prefix+bgBorn).Data());
-  TFile* f_bgBox_1  = TFile::Open((prefix+bgBox_1 ).Data());
-  TFile* f_bgBox_2  = TFile::Open((prefix+bgBox_2 ).Data());
-  TFile* f_bgBox_3  = TFile::Open((prefix+bgBox_3 ).Data());
-
-  TFile* f_bgQCDl = TFile::Open((prefix+bgQCDl ).Data());
-  TFile* f_bgQCD  = TFile::Open((prefix+bgQCD ).Data());
-  TFile* f_bgPJ   = TFile::Open((prefix+bgPJ ).Data());
+  TFile* f_train_file = TFile::Open((prefix+rootfile).Data());
 
   std::cout<<"  ... done"<<std::endl;
 
-  TString baseDir="RunLumiSelectionMod/MCProcessSelectionMod/HLTModP/GoodPVFilterMod/PhotonMvaMod/JetPub/JetCorrectionMod/PhotonPairSelectorPresel/PhotonTreeWriterPresel/";
+  TTree* t_signal_1 = (TTree*) f_train_file->Get((TString("gluglu_H_gg_124_pu2012")).Data());
+  TTree* t_signal_2 = (TTree*) f_train_file->Get((TString("vbf_H_gg_124_pu2012")).Data());
+  TTree* t_signal_3 = (TTree*) f_train_file->Get((TString("wz_H_gg_124_pu2012")).Data());
+  TTree* t_signal_4 = (TTree*) f_train_file->Get((TString("tt_H_gg_124_pu2012")).Data());
 
-  TTree* t_signal_1 = (TTree*) f_signal_1->Get((baseDir+TString("hPhotonTree")).Data());
-  TTree* t_signal_2 = (TTree*) f_signal_2->Get((baseDir+TString("hPhotonTree")).Data());
-  TTree* t_signal_3 = (TTree*) f_signal_3->Get((baseDir+TString("hPhotonTree")).Data());
-  TTree* t_signal_4 = (TTree*) f_signal_4->Get((baseDir+TString("hPhotonTree")).Data());
+  TTree* t_bgBorn   = (TTree*) f_train_file->Get((TString("diphotonjets")).Data());
+  TTree* t_bgBox_1  = (TTree*) f_train_file->Get((TString("box_10")).Data());
+  TTree* t_bgBox_2  = (TTree*) f_train_file->Get((TString("box_25")).Data());
+  TTree* t_bgBox_3  = (TTree*) f_train_file->Get((TString("box_250")).Data());
 
-  TTree* t_bgBorn   = (TTree*) f_bgBorn ->Get((baseDir+TString("hPhotonTree")).Data());
-  TTree* t_bgBox_1  = (TTree*) f_bgBox_1->Get((baseDir+TString("hPhotonTree")).Data());
-  TTree* t_bgBox_2  = (TTree*) f_bgBox_2->Get((baseDir+TString("hPhotonTree")).Data());
-  TTree* t_bgBox_3  = (TTree*) f_bgBox_3->Get((baseDir+TString("hPhotonTree")).Data());
-
-  TTree* t_bgQCDl = (TTree*) f_bgQCDl->Get((baseDir+TString("hPhotonTree")).Data());
-  TTree* t_bgQCD  = (TTree*) f_bgQCD->Get((baseDir+TString("hPhotonTree")).Data());
-  TTree* t_bgPJ   = (TTree*) f_bgPJ ->Get((baseDir+TString("hPhotonTree")).Data());
+  TTree* t_bgQCD30_pf = (TTree*) f_train_file->Get((TString("qcd_pf_30")).Data());
+  TTree* t_bgQCD30_ff = (TTree*) f_train_file->Get((TString("qcd_ff_30")).Data()); 
+  TTree* t_bgQCD40_pf = (TTree*) f_train_file->Get((TString("qcd_pf_40")).Data());
+  TTree* t_bgQCD40_ff = (TTree*) f_train_file->Get((TString("qcd_ff_40")).Data()); 
+  
+  TTree* t_bgGJET20_pf = (TTree*) f_train_file->Get((TString("gjet_pf_20")).Data());
+  TTree* t_bgGJET40_pf = (TTree*) f_train_file->Get((TString("gjet_pf_40")).Data());
 
   //-------------add new branch procidx to input tree-------------------
   float iproc = 0.;
   TBranch* procB_signal_1 = t_signal_1->Branch("procidx", &iproc, "procidx/F");//define new branch
   for(int i=1; i<=t_signal_1->GetEntries(); ++i)
     procB_signal_1->Fill();//fill the new branch
+
   iproc = 1.;
   TBranch* procB_signal_2 = t_signal_2->Branch("procidx", &iproc, "procidx/F");
   for(int i=1; i<=t_signal_2->GetEntries(); ++i)
     procB_signal_2->Fill();
+
   iproc = 2.;
   TBranch* procB_signal_3 = t_signal_3->Branch("procidx", &iproc, "procidx/F");
   for(int i=1; i<=t_signal_3->GetEntries(); ++i)
     procB_signal_3->Fill();
+
   iproc = 3.;
   TBranch* procB_signal_4 = t_signal_4->Branch("procidx", &iproc, "procidx/F");
   for(int i=1; i<=t_signal_4->GetEntries(); ++i)
     procB_signal_4->Fill();
+  
   iproc = 4.;
   TBranch* procB_bgBorn = t_bgBorn->Branch("procidx", &iproc, "procidx/F");
   for(int i=1; i<=t_bgBorn->GetEntries(); ++i)
     procB_bgBorn->Fill();
+
   iproc = 5.;
   TBranch* procB_bgBox_1 = t_bgBox_1->Branch("procidx", &iproc, "procidx/F");
   for(int i=1; i<=t_bgBox_1->GetEntries(); ++i)
     procB_bgBox_1->Fill();
+
   iproc = 6.;
   TBranch* procB_bgBox_2 = t_bgBox_2->Branch("procidx", &iproc, "procidx/F");
   for(int i=1; i<=t_bgBox_2->GetEntries(); ++i)
     procB_bgBox_2->Fill();
+
   iproc = 7.;
   TBranch* procB_bgBox_3 = t_bgBox_3->Branch("procidx", &iproc, "procidx/F");
   for(int i=1; i<=t_bgBox_3->GetEntries(); ++i)
     procB_bgBox_3->Fill();
+
   iproc = 8.;
-  TBranch* procB_bgQCDl = t_bgQCDl->Branch("procidx", &iproc, "procidx/F");
-  for(int i=1; i<=t_bgQCDl->GetEntries(); ++i)
-    procB_bgQCDl->Fill();
+  TBranch* procB_bgQCD30_pf = t_bgQCD30_pf->Branch("procidx", &iproc, "procidx/F");
+  for(int i=1; i<=t_bgQCD30_pf->GetEntries(); ++i)
+    procB_bgQCD30_pf->Fill();
+
   iproc = 9.;
-  TBranch* procB_bgQCD = t_bgQCD->Branch("procidx", &iproc, "procidx/F");
-  for(int i=1; i<=t_bgQCD->GetEntries(); ++i)
-    procB_bgQCD->Fill();  
+  TBranch* procB_bgQCD30_ff = t_bgQCD30_ff->Branch("procidx", &iproc, "procidx/F");
+  for(int i=1; i<=t_bgQCD30_ff->GetEntries(); ++i)
+    procB_bgQCD30_ff->Fill();
+
   iproc = 10.;
-  TBranch* procB_bgPJ = t_bgPJ->Branch("procidx", &iproc, "procidx/F");
-  for(int i=1; i<=t_bgPJ->GetEntries(); ++i)
-    procB_bgPJ->Fill();
-
-  //--------------get PU weights-----------------------------------------
-  puweights[0] = getpuweights(f_signal_1,hpuest);
-  puweights[1] = getpuweights(f_signal_2,hpuest);
-  puweights[2] = getpuweights(f_signal_3,hpuest);
-  puweights[3] = getpuweights(f_signal_4,hpuest);
-
-  puweights[4] = getpuweights(f_bgBorn,hpuest);
-  puweights[5] = getpuweights(f_bgBox_1,hpuest);
-  puweights[6] = getpuweights(f_bgBox_2,hpuest);
-  puweights[7] = getpuweights(f_bgBox_3,hpuest);
+  TBranch* procB_bgQCD40_pf = t_bgQCD40_pf->Branch("procidx", &iproc, "procidx/F");
+  for(int i=1; i<=t_bgQCD40_pf->GetEntries(); ++i)
+    procB_bgQCD40_pf->Fill();
   
-  puweights[8] = getpuweights(f_bgQCDl,hpuest);
-  puweights[9] = getpuweights(f_bgQCD,hpuest);
-  puweights[10] = getpuweights(f_bgPJ,hpuest);
+  iproc = 11.;
+  TBranch* procB_bgQCD40_ff = t_bgQCD40_ff->Branch("procidx", &iproc, "procidx/F");
+  for(int i=1; i<=t_bgQCD40_ff->GetEntries(); ++i)
+    procB_bgQCD40_ff->Fill();
    
-  //--------------get 1/Lumi_MC------------------------------------------
-  TH1D* hNE_signal_1 = (TH1D*) f_signal_1->FindObjectAny("hDAllEvents");//ming: where's it?
-  TH1D* hNE_signal_2 = (TH1D*) f_signal_2->FindObjectAny("hDAllEvents");
-  TH1D* hNE_signal_3 = (TH1D*) f_signal_3->FindObjectAny("hDAllEvents");
-  TH1D* hNE_signal_4 = (TH1D*) f_signal_4->FindObjectAny("hDAllEvents");
+  iproc = 12.;
+  TBranch* procB_bgGJET20_pf = t_bgGJET20_pf->Branch("procidx", &iproc, "procidx/F");
+  for(int i=1; i<=t_bgGJET20_pf->GetEntries(); ++i)
+    procB_bgGJET20_pf->Fill();
 
-  //double w_signal_1 = 0.03742/hNE_signal_1->GetBinContent(1);//ming:pb
-  //double w_signal_2 = 0.00286/hNE_signal_2->GetBinContent(1);
-  //double w_signal_3 = 0.00229/hNE_signal_3->GetBinContent(1);
-  //double w_signal_4 = 0.00022/hNE_signal_4->GetBinContent(1);
-  
-  double w_signal_1 = 0.03607/hNE_signal_1->GetBinContent(1);//ming:pb
-  double w_signal_2 = 0.00281/hNE_signal_2->GetBinContent(1);
-  double w_signal_3 = 0.00214/hNE_signal_3->GetBinContent(1);
-  double w_signal_4 = 0.00021/hNE_signal_4->GetBinContent(1);
+  iproc = 13.;
+  TBranch* procB_bgGJET40_pf = t_bgGJET40_pf->Branch("procidx", &iproc, "procidx/F");
+  for(int i=1; i<=t_bgGJET40_pf->GetEntries(); ++i)
+    procB_bgGJET40_pf->Fill();
 
-  TH1D* hNE_bgBorn = (TH1D*) f_bgBorn->FindObjectAny("hDAllEvents");
-  double w_bgBorn  = 1.15*154.7/hNE_bgBorn->GetBinContent(1);
-
-  TH1D* hNE_bgBox_1 = (TH1D*) f_bgBox_1->FindObjectAny("hDAllEvents");
-  double w_bgBox_1  = 1.3*358.2/hNE_bgBox_1->GetBinContent(1);
-
-  TH1D* hNE_bgBox_2 = (TH1D*) f_bgBox_2->FindObjectAny("hDAllEvents");
-  double w_bgBox_2  = 1.3*12.37/hNE_bgBox_2->GetBinContent(1);
-
-  TH1D* hNE_bgBox_3 = (TH1D*) f_bgBox_3->FindObjectAny("hDAllEvents");
-  double w_bgBox_3  = 1.3*0.00021/hNE_bgBox_3->GetBinContent(1);
-
-  TH1D* hNE_bgQCDl = (TH1D*) f_bgQCDl->FindObjectAny("hDAllEvents");
-  double w_bgQCDl  = 1.3*10868.0/hNE_bgQCDl->GetBinContent(1);  
-  
-  TH1D* hNE_bgQCD = (TH1D*) f_bgQCD->FindObjectAny("hDAllEvents");
-  double w_bgQCD  = 1.3*1.87E+07*0.0023/hNE_bgQCD->GetBinContent(1);//ming:0.0023 should be the double EM enriched filter efficiency
-
-  TH1D* hNE_bgPJ = (TH1D*) f_bgPJ->FindObjectAny("hDAllEvents");
-  double w_bgPJ  = 1.3*77100.*0.0065/hNE_bgPJ->GetBinContent(1);
- 
   //----------------------Train MVA-------------------------------------------
-  //TFile* tmvaOutput = new TFile("/scratch/bendavid/root/hggmvaJan2/tmvaOutputfpJan2.root","RECREATE");
-  TFile* tmvaOutput = new TFile("/home/mingyang/cms/root/RootFiles/hggmva_2012_jan16/Tmva_TrainOutput_SMDipho_2012Jan16.root","RECREATE");//root file to store training information
-  //TMVA::Factory* tmva = new TMVA::Factory("HggBambu_FP_Jan2",tmvaOutput);
-  TMVA::Factory* tmva = new TMVA::Factory("HggBambu_SMDipho_Jan16",tmvaOutput);
+  TFile* tmvaOutput = new TFile("/home/mingyang/cms/root/RootFiles/2012_ICHEP/DiphotonMVA/Tmva_TrainOutput_SMDipho_2012ICHEP_DijetTagCorrSmear_.root","RECREATE");
+  TMVA::Factory* tmva = new TMVA::Factory("Hgg_SMDipho_2012ICHEP_DijetTagCorrSmear",tmvaOutput);
   
   //add input signal tree
-  tmva->AddSignalTree(t_signal_1,w_signal_1);//each input sample is normalized to unit luminosity
-  tmva->AddSignalTree(t_signal_2,w_signal_2);
-  tmva->AddSignalTree(t_signal_3,w_signal_3);
-  tmva->AddSignalTree(t_signal_4,w_signal_4); 
+  tmva->AddSignalTree(t_signal_1);
+  tmva->AddSignalTree(t_signal_2);
+  tmva->AddSignalTree(t_signal_3);
+  tmva->AddSignalTree(t_signal_4); 
   
   //add input background tree
-  tmva->AddBackgroundTree(t_bgBorn,w_bgBorn);
-  tmva->AddBackgroundTree(t_bgBox_1,w_bgBox_1);
-  tmva->AddBackgroundTree(t_bgBox_2,w_bgBox_2);
-  tmva->AddBackgroundTree(t_bgBox_3,w_bgBox_3);
-  tmva->AddBackgroundTree(t_bgQCDl,w_bgQCDl);  
-  tmva->AddBackgroundTree(t_bgQCD,w_bgQCD);
-  tmva->AddBackgroundTree(t_bgPJ,w_bgPJ);
+  tmva->AddBackgroundTree(t_bgBorn);
+  tmva->AddBackgroundTree(t_bgBox_1);
+  tmva->AddBackgroundTree(t_bgBox_2);
+  tmva->AddBackgroundTree(t_bgBox_3);
+
+  tmva->AddBackgroundTree(t_bgQCD30_pf);
+  tmva->AddBackgroundTree(t_bgQCD30_ff);
+  tmva->AddBackgroundTree(t_bgQCD40_pf);
+  tmva->AddBackgroundTree(t_bgQCD40_ff);
+
+  tmva->AddBackgroundTree(t_bgGJET20_pf);  
+  tmva->AddBackgroundTree(t_bgGJET40_pf);
 
   //add training variable
-  tmva->AddVariable("masserrsmeared/mass",    'F');
-  tmva->AddVariable("masserrsmearedwrongvtx/mass",    'F');
-  tmva->AddVariable("vtxprob",    'F');  
-  tmva->AddVariable("ph1.pt/mass" ,  'F');
-  tmva->AddVariable("ph2.pt/mass" ,  'F');
-  tmva->AddVariable("ph1.eta" ,  'F');
-  tmva->AddVariable("ph2.eta" ,  'F');
-  tmva->AddVariable("TMath::Cos(ph1.phi-ph2.phi)" ,  'F');
-  tmva->AddVariable("ph1.idmva" ,  'F');
-  tmva->AddVariable("ph2.idmva" ,  'F');
+  tmva->AddVariable("sigmaMrvoM",    'D');
+  tmva->AddVariable("sigmaMwvoM",    'D');
+  tmva->AddVariable("vtxprob",    'D');  
+  tmva->AddVariable("ptoM1" ,  'D');
+  tmva->AddVariable("ptoM2" ,  'D');
+  tmva->AddVariable("eta1" ,  'D');
+  tmva->AddVariable("eta2" ,  'D');
+  tmva->AddVariable("cosphi" ,  'D');
+  tmva->AddVariable("idmva1" ,  'D');
+  tmva->AddVariable("idmva2" ,  'D');
 
-  //tmva->AddVariable("TMath::Sqrt(ph1.pt^2+ph2.pt^2+2.*ph1.pt*ph2.pt*TMath::Cos(ph1.scphi-ph2.scphi))/mass" ,  'F');
-
-  //add signal weight: 1/resolution*puweight*ptweight*presel eff scale factor* trigger eff
-  tmva->SetSignalWeightExpression("(vtxprob*mass/masserrsmeared + (1.0-vtxprob)*mass/masserrsmearedwrongvtx)*puweight(numPU,procidx)*ptweight(genHiggspt,procidx)*effweight(ph1.phcat)*effweight(ph2.phcat)*trigeffweight(ph1.phcat,ph2.phcat,ph1.pt,ph2.pt)"); 
-  //add background weight: it seems not necessary to include ptweight here...
-  //ming: apply no scale factor to fake fake; not sure how the scale factors determined though
-  tmva->SetBackgroundWeightExpression("((!ph1.ispromptgen && !ph2.ispromptgen)*1.0/1.3 + !(!ph1.ispromptgen && !ph2.ispromptgen)*1.0)*puweight(numPU,procidx)*ptweight(genHiggspt,procidx)*effweight(ph1.phcat)*effweight(ph2.phcat)*trigeffweight(ph1.phcat,ph2.phcat,ph1.pt,ph2.pt)");
+  //add signal weight: 1/lumi * 1/resolution
+  tmva->SetSignalWeightExpression("xsec_weight*(vtxprob/sigmaMrvoM + (1.0-vtxprob)/sigmaMwvoM)"); 
+  //add background weight: 1/lumi 
+  tmva->SetBackgroundWeightExpression("xsec_weight");
   
-  TCut selectionCut = "ph1.pt/mass > 40./120 && ph2.pt/mass > 30./120. && mass < 180. && mass > 100. && ph1.idmva>-0.3 && ph2.idmva>-0.3";
-  //dec9id
-  //tmva->PrepareTrainingAndTestTree(selectionCut,"nTrain_Signal=151800:nTrain_Background=89700:");
-  //tmva->PrepareTrainingAndTestTree(selectionCut,"nTrain_Signal=95400:nTrain_Background=89700:");
-  //tmva->PrepareTrainingAndTestTree(selectionCut,"SplitMode=Alternate");
-  tmva->PrepareTrainingAndTestTree(selectionCut,"nTrain_Signal=0:nTrain_Background=0:nTest_Signal=10:nTest_Background=10");
+  TCut selectionCut = "ptoM1 > 40./120 && ptoM2 > 30./120. && mass < 180. && mass > 100. && idmva1>-0.3 && idmva2>-0.3";
+ 
+  TCut diphojCut = "!( (procidx==4) && ((event>=270000 && event<=390000) || (event>=480000 && event<=510000) || (event>=1170000 && event<=1200000) || (event>=1230000 && event<=1260000)  || (event>=1890000 && event<=1920000)  || (event>=2670000 && event<=2730000) || (event>=3090000 && event<=3210000) || (event>=3270000 && event<=3300000) || (event>=3510000 && event<=3600000) || (event>=4890000 && event<=5010000) || (event>=5340000 && event<=5370000) || (event>=5430000 && event<=5500000)))";
+ 
+  TCut totalCut = selectionCut && diphojCut;
+
+  tmva->PrepareTrainingAndTestTree(totalCut,"nTrain_Signal=0:nTrain_Background=0:nTest_Signal=20:nTest_Background=20");
   tmva->BookMethod( TMVA::Types::kBDT, "BDTG" ,
 		    "!H:!V:NTrees=2000::BoostType=Grad:Shrinkage=0.1:UseBaggedGrad=F:nCuts=2000:MaxDepth=3:NNodesMax=100000:UseYesNoLeaf=F:nEventsMin=10000:");
-  
-  //tmva->BookMethod(TMVA::Types::kLD, "LD", "H:!V:VarTransform=None:CreateMVAPdfs:PDFInterpolMVAPdf=Spline2:NbinsMVAPdf=50:NsmoothMVAPdf=10");
   
   tmva->TrainAllMethods();
   tmva->TestAllMethods();
